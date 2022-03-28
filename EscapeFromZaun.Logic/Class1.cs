227ed530fe6067159b3bf0279627f0f55c71430b
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2022-03-28 20:30:30 +0200

    -Logic project created -MainMenu view model created

---
+++ EscapeFromZaun.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EscapeFromZaun.Logic
+{
+    public class Class1
+    {
+    }
+}
--- EscapeFromZaun/ViewModels/MainMenuWindowViewModel.cs
+++ EscapeFromZaun/ViewModels/MainMenuWindowViewModel.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace EscapeFromZaun.ViewModels
 {
-    public class MainMenuWindowViewModel
+    public class MainMenuWindowViewModel : ObservableRecipient
     {
+        public ICommand PlayCommand { get; set; }
+        public ICommand SettingsCommand { get; set; }
+        public ICommand QuitCommand { get; set; }
+        public string BackgroundImage { get; set; }
+
+        public MainMenuWindowViewModel()
+        {
+            PlayCommand = new RelayCommand(() => this.PlayButtonClick());
+            SettingsCommand = new RelayCommand(() => this.SettingsButtonClick());
+            QuitCommand = new RelayCommand(() => this.QuitButtonClick());
+
+        }
+
+        private void QuitButtonClick()
+        {
+            Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive).Close();
+        }
+
+        private void SettingsButtonClick()
+        {
+            Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive).DataContext = new SettingsMenuWindowViewModel();
+        }
+
+        private void PlayButtonClick()
+        {
+            Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive).DataContext = new PlayMenuWindowViewModel();
+        }
     }
 }
