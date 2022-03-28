2a67fda136403663258f80ecd81a9328491e454c
nemeth.donat.16@stud.uni-obuda.hu <nemeth.donat.16@stud.uni-obuda.hu>
2022-03-28 20:21:38 +0200

    Images added to the project

--- EscapeFromZaun/MainWindow.xaml.cs
+++ EscapeFromZaun/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace EscapeFromZaun
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainMenuWindowViewModel();
+            this.DataContext = new PlayMenuWindowViewModel();
         }
     }
 }
