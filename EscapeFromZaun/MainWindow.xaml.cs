c1c10f32830bf5c4a86ab2bbc10fc147925811cb
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2022-03-28 12:31:17 +0200

    Views connected

--- EscapeFromZaun/MainWindow.xaml.cs
+++ EscapeFromZaun/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EscapeFromZaun.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace EscapeFromZaun
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainMenuWindowViewModel();
         }
     }
 }
