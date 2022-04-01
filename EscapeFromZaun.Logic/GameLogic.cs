5131694309094b7d8db5a412a87a83c269432501
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2022-04-01 13:08:17 +0200

    Model layer created Player model created GameLogic created

---
+++ EscapeFromZaun.Logic/GameLogic.cs
@@ -0,0 +1,14 @@
+﻿using EscapeFromZaun.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeFromZaun.Logic
+{
+    public class GameLogic
+    {
+        public PlayerModel Player { get; set; }
+    }
+}
---
+++ EscapeFromZaun.Model/PlayerModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EscapeFromZaun.Model
+{
+    public class PlayerModel
+    {
+        public string Name { get; set; }
+        public string Outcome { get; set; }
+        public DateTime PlayerRunTime { get; set; }
+    }
+}
