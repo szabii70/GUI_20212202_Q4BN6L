d8ebb258dcdf94a8d2e87cba37c22ce2d861d5ff
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2022-04-05 21:48:43 +0200

    Player logic created, not all property implemented

--- EscapeFromZaun.WpfLogic/GameLogic.cs
+++ EscapeFromZaun.WpfLogic/GameLogic.cs
@@ -5,6 +5,13 @@ namespace EscapeFromZaun.WpfLogic
 {
     public class GameLogic : IGameLogic
     {
+        bool goingLeft = false;
+        bool goingRight = false;
+        bool jumping;
+        int force = 20;
+        int jumpSpeed = 10;
         public PlayerModel Player { get; set; }
+        public enum Directions { left,right }
+        
     }
 }
---
+++ EscapeFromZaun.WpfLogic/PlayerLogic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using static EscapeFromZaun.WpfLogic.GameLogic;
+
+namespace EscapeFromZaun.WpfLogic
+{
+    public class PlayerLogic
+    {
+        int width;
+        int height;
+        public int DrawFromX { get; set; }
+        public int DrawFromY { get; set; }
+        public PlayerLogic(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public Brush PlayerBrush
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public Geometry Hitbox
+        {
+            get
+            {
+                return new RectangleGeometry(new System.Windows.Rect(DrawFromX, DrawFromY, width, height));
+            }
+        }
+        public Geometry CharacterFrame
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public void Move(Directions direction)
+        {
+            switch (direction)
+            {
+                case Directions.right:
+                    DrawFromX += 10;
+                    break;
+                case Directions.left:
+                    DrawFromX -= 10;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public bool IsCollision(Geometry otherArea)
+        {
+            return Geometry.Combine(this.Hitbox, otherArea, GeometryCombineMode.Intersect, null).GetArea() > 0;
+        }
+    }
+}
