5f7d0e5a07a841b9ae3d68b65c94e89ce4c72bd5
nemeth.donat.16@stud.uni-obuda.hu <nemeth.donat.16@stud.uni-obuda.hu>
2022-04-05 12:30:00 +0200

    PlayerMenu viewmodel ObservableCollection setup

--- EscapeFromZaun.Model/PlayerModel.cs
+++ EscapeFromZaun.Model/PlayerModel.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 
 namespace EscapeFromZaun.Model
 {
-    public class PlayerModel
+    public class PlayerModel : ObservableObject
     {
         public string Name { get; set; }
         public string Outcome { get; set; }
--- EscapeFromZaun/ViewModels/PlayMenuWindowViewModel.cs
+++ EscapeFromZaun/ViewModels/PlayMenuWindowViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using EscapeFromZaun.Logic;
+using EscapeFromZaun.Model;
 using GalaSoft.MvvmLight.Command;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +18,32 @@ namespace EscapeFromZaun.ViewModels
     {
 
         IScoreSerializationLogic logic;
+
+        public ObservableCollection<PlayerModel> Players { get; set; }
+
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public PlayMenuWindowViewModel() : this(IsInDesignMode ? null : Ioc.Default.GetService<IScoreSerializationLogic>())
+        {
+
+        }
+
         public PlayMenuWindowViewModel(IScoreSerializationLogic logic)
         {
             this.logic = logic;
+            Players = new ObservableCollection<PlayerModel>();
 
+            logic.DeSerialize("C:/Users/User/Desktop/Game.json");
+
+            logic.SetupColllections(Players);
 
             this.NewGameClick = new RelayCommand(() => this.NewGameClick_Button());
             this.LoadGameClick = new RelayCommand(() => this.LoadGameClick_Button());
