094f1be1ccb092e0404582caa987762f573219dd
nemeth.donat.16@stud.uni-obuda.hu <nemeth.donat.16@stud.uni-obuda.hu>
2022-04-14 14:36:35 +0200

    Bullet class implemented

---
+++ EscapeFromZaun.WpfLogic/Bullet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace EscapeFromZaun.WpfLogic
+{
+    public class Bullet
+    {
+        public Bullet(System.Drawing.Point from, Vector to)
+        {
+            Center = from;
+            Speed = to;
+        }
+
+        public System.Drawing.Point Center { get; set; }
+
+        public Vector Speed { get; set; }
+
+        public Brush ItemBrush
+        {
+            get
+            {
+                return Brushes.Yellow;
+            }
+
+        }
+
+
+
+        public bool Move(System.Drawing.Size area)
+        {
+            //hova kerülne a lépéskor a lövedék
+            System.Drawing.Point newCenter =
+                new System.Drawing.Point(Center.X + (int)Speed.X,
+                Center.Y + (int)Speed.Y);
+            if (newCenter.X >= 0 &&
+                newCenter.X <= area.Width &&
+                newCenter.Y >= 0 &&
+                newCenter.Y <= area.Height
+                )
+            {
+                //pályán belül van a lövedék
+                Center = newCenter;
+                return true;
+            }
+            else
+            {
+                //épp elhagyta a pályát
+                return false;
+            }
+        }
+    }
+}
